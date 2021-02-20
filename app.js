ecc79157e4ef6c553a94264e43f767ac11eb8714
Antoinette <antoinettemedlock@outlook.com>
2021-02-19 21:04:22 -0500

    add basic styles

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const form = document.querySelector("form")
 
 form.addEventListener("submit", function(event){
     if(event.tagName === "BUTTON"){
-        
+        let newDiv = document.createElement("div")
+
+        form.appendChild(newDiv)
     }
 })
